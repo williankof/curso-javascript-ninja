9b2687b06241990fa46a3f5ae16bce3f2cc8fb9c
williankof <willian.kof@gmail.com>
2020-09-09 12:28:08 -0300

    Challenge 08 [Correçao]

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -12,7 +12,10 @@ Invoque a função criada acima, passando dois números que serão somados, e mo
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-console.log('A soma de ' + 10 + ' e ' + 20 + ' é igual a ' + sum(10, 20) + '.');
+
+let value1 = 10
+let value2 = 20
+console.log('A soma de ' + value1 + ' e ' + value2 + ' é igual a ' + sum(value1, value2) + '.');
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
@@ -25,7 +28,7 @@ Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
 function showName() {
-  return showName.name;
+  return 'William Bispo';
 }
 
 /*
@@ -38,7 +41,7 @@ Usando a variável criada acima, mostre no console o nome e o retorno da funçã
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função ' + showName.name + ' retorna ' + varShowName() + '.');
+console.log('A função ' + varShowName.name + ' retorna ' + varShowName() + '.');
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
