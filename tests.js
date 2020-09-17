6ba9b22851d2a5f1fdc82fea3eebb0ecf4673902
williankof <willian.kof@gmail.com>
2020-09-17 12:41:25 -0300

    Challenge-11

--- tests.js
+++ tests.js
@@ -1,58 +1,38 @@
-operation = {
-    '+': function(n1, n2) {return n1 + n2;},
-    '-': function(n1, n2) {return n1 - n2;},
-    '*': function(n1, n2) {return n1 * n2;},
-    '/': function(n1, n2) {return n1 / n2;},
-    '%': function(n1, n2) {return n1 % n2;},
+/* 
+let car = {
+    brand: 'VW',
+    model: 'Gol',
+    year: 2013
+
 };
 
-
-function isOperatorValid(param) {
-    param === '+' || param === '-' || param === '*' || param === '/' || param === '%' ? param = true : param = false
-    return param;
+for(let i in car) {
+    console.log(i);
 }
 
-function calculator(operador) {
-    if(isOperatorValid(operador) === false) {
-        return false
-    } else {
-        return function(p1, p2) {
-            if(typeof p1 !== 'number' || typeof p2 !== 'number'){
-                return false;
-            }
-            return operation[operador](p1, p2);
-        }
+
+for(let x in car) {
+    console.log(x, '=>' ,car[x]);
+}
+
+
+
+for(let i = 0; i < 10; i++) {
+    if(i === 5) {
+        break;
     }
+    console.log(i)
+
 }
+console.log('End of for')
+*/
 
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr2 = ['a', 'b', 'c', 'd', 'd', 'f', 'g'];
 
-
-function showOperationMessage(number1, operator, number2) {
-    return 'A operação ' + number1 + ' ' + operator + ' ' + number2 + ' = ';
+for(let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === 'd' || arr2[i] === 'b') {
+        continue;
+    }
+    console.log(arr2[i]);
 }
-
-function showErrorMessage(param) {
-    if(calculator(param) === 'false') {
-        return 'Oparação ' + param + ' não permitida!';
-    } 
-}
-
-let number1 = 0;
-let number2 = 0;
-let operationSignal;
-
-operationSignal = 'oi';
-let sum = calculator(operationSignal)
-
-
-number1 = 5
-number2 = 10
-//console.log(sum(number1, number2))
-console.log(showOperationMessage(number1, operationSignal, number2), sum(number1, number2))
-
-console.log(isOperatorValid(operationSignal))
-//console.log(calculator('b')(5, 10))
-
-// if(isOperatorValid(operationSignal) === 'false') {
-//     console.log('falso')
-// }
